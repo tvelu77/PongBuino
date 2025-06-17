ca6a66900bda9852d3d299703c0e786c1549cea6
Thomas VELU <velu.thomas@murena.io>
2025-06-17 10:19:18 +0200

    feat: add ball's physics logic

--- pongbuino/pongbuino.ino
+++ pongbuino/pongbuino.ino
@@ -1,14 +1,32 @@
 #include <Gamebuino-Meta.h>
 
+int xPosition = 32;
+int yPosition = 32;
+int xSpeed = 1;
+int ySpeed = 1;
+int ballWidth = 4;
+int ballHeight = 4;
+
 void setup() {
   gb.begin();
 }
+
 
 void loop() {
   while (!gb.update()) {
     gb.display.clear();
   }
-  gb.display.print("WIP ;)\nThis program will be a classic pong !");
-  gb.display.setColor(LIGHTBLUE);
-  gb.display.print("\n- tvelu77");
+
+  if (xPosition >= gb.display.width() - ballWidth || xPosition <= 0) {
+    xSpeed = -xSpeed;
+  }
+
+  if (yPosition >= gb.display.height() - ballHeight || yPosition <= 0) {
+    ySpeed = -ySpeed;
+  }
+
+  xPosition = xPosition + xSpeed;
+  yPosition = yPosition + ySpeed;
+
+  gb.display.fillRect(xPosition, yPosition, ballHeight, ballWidth);
 }
